114be864e4464b9447c6a3d31de7fed5d1867beb
Martha Agu <agumartha053@gmail.com>
2025-09-28 17:23:03 +0100

    bug fix

--- src/routes/Index.jsx
+++ src/routes/Index.jsx
@@ -1,21 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from "react";
-import Login from "../pages/authentication/Login";
-import Home from "../pages/dashboard/Home";
-import Analytics from "../pages/dashboard/Body/Analytics";
-import Users from "../pages/dashboard/Body/Users";
+import Login from "../pages/Authentication/Login";
+import Home from "../pages/Dashboard/Home";
+import Analytics from "../pages/Dashboard/Body/Analytics";
+import Users from "../pages/Dashboard/Body/Users";
 import Register from "../pages/Authentication/Register";
 
 const Index = () => {
   return (
-    // <Router>
-    //   <Routes>
-    //     <Route path="/" element={<Login />} />
-    //     <Route path="/home" element={<Home />} />
-    //     <Route path="/analytics" element={<Analytics />} />
-    //     <Route path="/users" element={<Users />} />
-    //   </Routes>
-    // </Router>
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
